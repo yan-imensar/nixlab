d3a304d30daf04bc3f0715c178a0b1329dec1969
Yan IMENSAR <sub@yan-imensar.fr>
2026-02-09 17:04:06 +0100

    sync

--- nixos/modules/k3s/server.nix
+++ nixos/modules/k3s/server.nix
@@ -1,9 +1,11 @@
 { lib, pkgs, ... }:
 {
   networking.firewall.allowedTCPPorts = [
-    6443 # k3s: API server
-    2379 # k3s: etcd clients
-    2380 # k3s: etcd peers
+    6443  # k3s: API server
+    2379  # k3s: etcd clients
+    2380  # k3s: etcd peers
+    9345  # k3s: supervisor API (HA)
+    10250 # k3s: Kubelet metrics/API
   ];
   networking.firewall.allowedUDPPorts = [
     8472 # k3s : Flannel
