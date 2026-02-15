b3f80768bd0a0f78a1a522f53d424784e13b80e4
Yan IMENSAR <sub@yan-imensar.fr>
2026-02-15 11:22:13 +0100

    sync

--- nixos/nodes/knix-0/configuration.nix
+++ nixos/nodes/knix-0/configuration.nix
@@ -5,6 +5,7 @@
     ../../modules/common/apps.nix
     ../../modules/k3s/server.nix
     ../../modules/storage/longhorn.nix
+    ../../modules/common/configuration.nix
   ];
 
   networking.hostName = "knix-0";
@@ -35,16 +36,6 @@
       "--node-ip 192.168.1.100"
     ];
   };
-
-  services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys =
-  [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDTqg1h617IHbN4fUsyfSf42uX68xTcbleAMWyRN/VEDntc4ESAi4H8GnmTiJmjiKXT/DD119XkoVWaVStqgZfey1H8kjVi6EzKEFXs2Y6Tnz1Ad+tQwcgpIl/vvVHVSPSMu9J1VQIYGpE+wYEZf0NTuQHu1bBS0oEOKLv63nUIVystBuchy8pJg9fAPInWxqfU12hoZUWtx6ZLaO6h9hatiqxJht81pOwjv2PW8vflIYKDnMYjzqVzgPipqo7e3P7aKpqaCT6jgVkKnFXz72mNcvWsS0YoD2GxrE3qAGb9UWOV5Y+2oxjAlqILas6ejUQ/s3swnJDBRMHY82QwCTZpdq2RJm+pHFZf/lRX5RQpgiNURj2+YOl3NQaEjE4a7ZEBK2Gos72tXss/5stlDTsWpI445NXYOq8uk7yrkxKf1dKAJPNGzGR4kcB4iyvcZw5noAn1HbmpdK5W1u1ms65z9PV+RdVo7AlJnHdSe1lPxe+erepWI5UJpVxP9I/OkMs= bomal@nixos"
-  ];
-  
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 8;
-  boot.loader.efi.canTouchEfiVariables = true;
 
   system.stateVersion = "24.05";
 }
